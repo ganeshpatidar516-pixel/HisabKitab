7108b41fd4b2d0ff9285229c4bbc5af44c4be1d2
GaneshPatidar <ganeshpatidar516@gmail.com>
2026-03-12 11:26:33 +0530

    AI controller updated for new router

--- ai/controller.py
+++ ai/controller.py
@@ -1,21 +1,13 @@
 from ai.router import route_message
-from ai.ai_execute import execute_action
-
 
 def ai_controller(message: str, customers: list):
 
-    # Step 1 — Understand message
     routed = route_message(message, customers)
 
     intent = routed["intent"]
     entities = routed["entities"]
 
-    # Step 2 — Execute action
-    result = execute_action(intent, entities)
-
     return {
-        "command": message,
         "intent": intent,
-        "entities": entities,
-        "result": result
+        "entities": entities
     }
